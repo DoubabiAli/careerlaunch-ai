6b70f3524ceb6f034d7e0ec347a3205a86e2d418
DoubabiAli <doubabiali@gmail.com>
2026-07-01 16:44:08 +0100

    Remove temporary API test file

--- backend/test_api.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://jsonplaceholder.typicode.com/posts/1"
-
-response = requests.get(url)
-
-print("Code HTTP :", response.status_code)
-print("Réponse :")
-print(response.json())
